f7006de4519402bf223bc7bbd85d79248a268ba5
geo1310 <gbriche59@yahoo.fr>
2024-06-28 12:21:35 +0200

    Create app lettings + Migrations

--- lettings/models.py
+++ lettings/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinLengthValidator
-from django.contrib.auth.models import User
 
 
 class Address(models.Model):
@@ -12,22 +11,13 @@ class Address(models.Model):
     country_iso_code = models.CharField(
         max_length=3, validators=[MinLengthValidator(3)]
     )
-
     def __str__(self):
         return f"{self.number} {self.street}"
-
+    
 
 class Letting(models.Model):
     title = models.CharField(max_length=256)
-    address = models.OneToOneField(Address, on_delete=models.CASCADE, related_name="+")
-
+    address = models.OneToOneField(Address, on_delete=models.CASCADE)
     def __str__(self):
         return self.title
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    favorite_city = models.CharField(max_length=64, blank=True)
-
-    def __str__(self):
-        return self.user.username
+    
